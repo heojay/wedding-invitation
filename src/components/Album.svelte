99e8e027010b312671be24c4dcd4afa560e967c8
Heo Jaewon <yonseijaewon@gmail.com>
2022-10-31 22:39:16 +0900

    Remove splider

--- src/components/Album.svelte
+++ src/components/Album.svelte
@@ -1,65 +1,22 @@
 <script lang="ts">
-	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-	import { onMount } from 'svelte';
 	import { generateSlides } from '$lib/generateSlides';
-	import '@splidejs/svelte-splide/css/default';
-
-	let main: Splide;
-	let thumbs: SplideSlide;
-
 	const slides = generateSlides();
-
-	const mainOptions = {
-		type: 'loop',
-		perPage: 1,
-		perMove: 1,
-		gap: '1rem',
-		pagination: false,
-		height: '100%'
-	};
-
-	const thumbsOptions = {
-		type: 'slide',
-		rewind: true,
-		gap: '1rem',
-		pagination: false,
-		fixedWidth: 110,
-		fixedHeight: 70,
-		cover: true,
-		focus: 'center' as const,
-		isNavigation: true,
-		updateOnMove: true
-	};
-
-	onMount(() => {
-		if (main && thumbs) {
-			main.sync(thumbs.splide);
-		}
-	});
 </script>
 
 <section>
 	<div class="container px-5 py-5 mx-auto">
 		<h1 class="mb-10 text-4xl font-bold">사진첩</h1>
-		<div class="flex-wrap">
-			<div class="m-auto max-w-3xl">
-				<Splide options={mainOptions} bind:this={main} aria-labelledby="thumbnails-example-heading">
-					{#each slides as slide}
-						<SplideSlide>
-							<img src={slide.src} alt={slide.alt} />
-						</SplideSlide>
-					{/each}
-				</Splide>
-			</div>
-			<div class="m-auto max-w-3xl">
-				<Splide options={thumbsOptions} bind:this={thumbs}>
-					{#each slides as slide}
-						<SplideSlide>
-							<img src={slide.src} alt={slide.alt} />
-						</SplideSlide>
-					{/each}
-				</Splide>
-			</div>
+		<div class="carousel w-full">
+			{#each slides as slide}
+				<div id={slide.alt} class="carousel-item w-full">
+					<img src={slide.src} class="w-full" />
+				</div>
+			{/each}
+		</div>
+		<div class="flex justify-center w-full pt-5 gap-3">
+			{#each slides as slide, i}
+				<a href="#{slide.alt}" class="btn btn-xs">{i + 1}</a>
+			{/each}
 		</div>
 	</div>
 </section>
--- src/lib/generateSlides.ts
+++ src/lib/generateSlides.ts
@@ -6,12 +6,12 @@
  *
  * @return An array with objects for sample images.
  */
-export function generateSlides(length = 10, sig = 0): Array<{ src: string; alt: string }> {
+export function generateSlides(length = 8, sig = 0): Array<{ src: string; alt: string }> {
 	return Array.from({ length }).map((_, index) => {
 		index = sig || index;
 
 		return {
-			src: `https://source.unsplash.com/random/800x450?sig=${index}`,
+			src: `https://via.placeholder.com/300x400/1`,
 			alt: `Image ${index}`
 		};
 	});
